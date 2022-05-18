06aeaf79e5712c2ee2fb3e4386378bcec2260b76
Dzero-GT <dillongustafson2002@gmail.com>
2022-05-17 21:21:14 -0500

    complete (conversion to grid was needed)

--- etchScript.js
+++ etchScript.js
@@ -1,24 +1,51 @@
-const container = document.querySelector('.container');
-let k = 0;
-for (let i = 0; i < 16; i++){
-    const column = document.createElement('div');
-    column.setAttribute('class' , 'column' );
-    for (let j = 0; j < 16; j++){
-        const row = document.createElement('div');
-        row.setAttribute('class' , 'row' );
-        row.setAttribute('data-type' , 'pixel');
-        column.appendChild(row);
-    }
-container.appendChild(column);
+//functions
+
+// makes grid item
+function generateGrid(){
+    const gridItem = document.createElement('div');
+    gridItem.classList.add('gridItem');
+    container.appendChild(gridItem);
 }
-const pixels = document.querySelectorAll('div[data-type=pixel]');
-pixels.forEach(highlight);
+//makes grid sizing
+function generate(){
+    size = prompt('what x by x dimension do you want the grid to be? ');
+
+    container.style.gridTemplateColumns = `repeat(${size}, auto)`;
+    container.style.gridTemplateRows = `repeat(${size}, auto)`;
+    container.style.display = 'inline-grid';
+
+    for (let i = 0; i < (Math.pow(size, 2)); i++){
+        generateGrid();
+    }
+    const gridList = document.querySelectorAll('.gridItem');
+    gridList.forEach(highlight);    
+}
+
 
 function highlight(e){
     e.addEventListener('mouseover', changeColor);
 }
 function changeColor(e) {
-    
-    this.setAttribute('class', 'row-selected');
+    this.classList.add('selected');
 }
 
+function resetPrompt() {
+    const gridList = document.querySelectorAll('.gridItem');
+    for (let i = 0; i < (Math.pow(size, 2)); i++) {
+        gridList[i].classList.remove('activated');
+        container.removeChild(gridList[i]);
+        container.style.display = 'none';
+    }
+    size = 0;
+    generate();
+}
+
+
+
+// Flow
+
+
+const container = document.querySelector('.container');
+generate();
+const reset = document.querySelector('.reset');
+reset.addEventListener('click',resetPrompt);
