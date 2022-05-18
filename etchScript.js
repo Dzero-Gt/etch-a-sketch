9254717fdb656aadf3953286cc1985d6b1f33935
Dzero-GT <dillongustafson2002@gmail.com>
2022-05-17 19:09:42 -0500

    highlight start complete

--- etchScript.js
+++ etchScript.js
@@ -1,8 +1,8 @@
 const container = document.querySelector('.container');
+let k = 0;
 for (let i = 0; i < 16; i++){
     const column = document.createElement('div');
     column.setAttribute('class' , 'column' );
-    column.setAttribute('data-type' , 'pixel');
     for (let j = 0; j < 16; j++){
         const row = document.createElement('div');
         row.setAttribute('class' , 'row' );
@@ -11,3 +11,14 @@ for (let i = 0; i < 16; i++){
     }
 container.appendChild(column);
 }
+const pixels = document.querySelectorAll('div[data-type=pixel]');
+pixels.forEach(highlight);
+
+function highlight(e){
+    e.addEventListener('mouseover', changeColor);
+}
+function changeColor(e) {
+    
+    this.setAttribute('class', 'row-selected');
+}
+
